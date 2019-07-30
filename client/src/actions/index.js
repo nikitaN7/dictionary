3e97784743ef59bebc0ffb42b527efbd801547fb
Nikita.N <nickitenko.job@gmail.com>
2019-07-30 20:26:21 +0300

    Add functions for put, update and delete words from DB, split fn to action creators and fn with axios request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,7 @@
+import axios from 'axios';
+import store from '../store';
+import { getMaxId } from '../function/getMaxId';
+
 const wordsLoaded = (newWords) => {
   return {
     type: 'FETCH_WORDS_SUCCESS',
@@ -19,27 +23,79 @@ const fetchWords = () => (dispatch) => {
     .catch((err) => dispatch(wordsError(err)));
 };
 
-const wordAdd = (wordId, wordData) => {
+const wordAddAction = (wordId, wordData) => {
   return {
     type: 'BOOK_ADD_TO_LIST',
     wordId: wordId,
     wordData: wordData
-  };
+  }
+}
+
+const wordAdd = (word, wordData) => (dispatch) => {
+
+  const { words } = store.getState().wordList;
+  const newId = getMaxId(words) + 1;
+
+  axios.post('/api/putData', {
+    ...wordData,
+    id: newId
+  })
+  .then((res) => {
+    if (res.statusText === 'OK') {
+      dispatch(fetchWords())
+      dispatch(wordAddAction(word.id, wordData))
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 };
 
-const wordUpdate = (wordId, wordData) => {
+const wordUpdateAction = (wordId, wordData) => {
   return {
     type: 'BOOK_UPDATE_IN_LIST',
     wordId: wordId,
     wordData: wordData
-  };
+  }
+}
+
+const wordUpdate = (word, wordData) => (dispatch) => {
+  axios.post('/api/updateData', {
+    id: word._id,
+    update: { ...wordData },
+  })
+  .then((res) => {
+    if (res.statusText === 'OK') {
+      dispatch(wordUpdateAction(word.id, wordData))
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 };
 
-const wordDelete = (wordId) => {
+const wordDeleteAction = (wordId) => {
   return {
     type: 'BOOK_DELETE_FROM_LIST',
     wordId: wordId
   };
+};
+
+const wordDelete = (word) => (dispatch) => {
+  axios.delete('/api/deleteData', {
+    data: {
+      id: word._id
+    }
+  })
+  .then((res) => {
+    if (res.statusText === 'OK') {
+      dispatch(wordDeleteAction(word.id))
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 };
 
 export {
---
+++ client/src/function/getMaxId.js
@@ -0,0 +1,4 @@
+export const getMaxId = (arr) => {
+  const idList = arr.map(item => item.id);
+  return Math.max(...idList, 0);
+}
