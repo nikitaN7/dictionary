8448f5b01eda8c4dfc829981b2a9471ad550f472
Nikita.N <nickitenko.job@gmail.com>
2019-10-25 13:29:13 +0300

    Fix words search fn and make refactoring to words filter fn

--- client/src/function/filterWordsByType.js
+++ client/src/function/filterWordsByType.js
@@ -3,7 +3,7 @@ import { filterList } from './filterList';
 export const filterWordsByType = (words, type) => {
   const bookmarksFilter = word => word.bookmarks === true;
 
-  if (type === 'hard-words') {
+  if (type !== 'all-words') {
     return filterList(words, bookmarksFilter);
   }
 
--- client/src/function/searchWordsByStr.js
+++ client/src/function/searchWordsByStr.js
@@ -1,14 +1,17 @@
 import { filterList } from './filterList';
 
 export const searchWordsByStr = (words, str) => {
-  if (str === '') {
-    return words;
-  }
-
   const searchFilter = word => {
-    const { en, ru } = word;
-    return en.indexOf(str) !== -1 || ru.indexOf(str) !== -1;
+    const translates = [word.en, word.ru];
+
+    return translates.some(item => {
+      return item.toLowerCase().indexOf(str.toLowerCase()) !== -1;
+    });
   };
 
-  return filterList(words, searchFilter);
+  if (str !== '') {
+    return filterList(words, searchFilter);
+  }
+
+  return words;
 };
